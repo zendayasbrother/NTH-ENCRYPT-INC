40ab8968458e3daaa9820c6159ef90124470b978
Daniel Onyeakazi <do4009@hw.ac.uk>
2026-05-09 12:34:39 +0100

    added seperate intefaces for user type

--- engine.py
+++ engine.py
@@ -16,17 +16,21 @@ class ManagementSystem(AuthSystem):
             print(f"\nLogin Successful! Welcome: {first_name} // ({user_type.upper()})")
             if user_type == "Admin":
                 # Admins can also view and accept / decline proposals sent by talent before deciding to search up and manipulate tables and records
-                self.display_interface() 
+                self.admin_interface() 
             else:
                 # Talent can only view their own records and can create proposals via algorithmic form; Pairings, Genre, Budget, etc.
-                print(f"Accessing {user_type} Dashboard...")
-                print()
+                self.talent_interface()
         else:
             print("\nAccess Denied.")
 
-    def display_interface(self):
+    def admin_interface(self):
+        # Insert the proposal management interface here, allowing users to view and manage proposals, as well as search and manipulate tables and records.
         name = input("Enter the table name to display: ")
         self.display_table(name)
+        
+    def talent_interface(self):
+        # Insert the talent dashboard interface here, allowing users to view their own records and create proposals.
+        print(f"Accessing your records...")
         
         
         
